5cd49d22f2a44c111c6621ca0a5599b108480fd7
CozyGunG <CozyGunG@users.noreply.github.com>
2021-07-10 00:13:29 +1200

    Added word_count probabilities in Naive Bayesian algorithm under buttons.py

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # SpamFilterDetector
-Uses Naive Bayesian algorithm to differentiate spam mails to non-spam mails
+Uses Naive Bayesian algorithm to differentiate spam mails to non-spam mails.  
+NLTK Stemming was removed from the implementation due to inconsistencies.  
 
-## Before Running
+
+# Before Running
 - asda
--- buttons.py
+++ buttons.py
@@ -6,6 +6,11 @@ Author: Alex Kim
 import tkinter as tk
 from tkinter import filedialog
 import pandas as pd
+from threading import Thread
+'''from PyQt5.QtWidgets import (
+    QApplication,
+    QLabel
+)'''
 import globalVar
 
 
@@ -21,7 +26,7 @@ class OpenFileBtn(tk.Button):
             self.filename = "/"
 
     def openFile(self):
-        self.filename = filedialog.askopenfilename(initialdir="/", title="Select A File",
+        self.filename = filedialog.askopenfilename(initialdir="/Users/alexk/Compsci 361/Assignment3", title="Select A File",
                                                    filetypes=(("CSV files", "*.csv"), ("All Files", "*.*")))
 
     def process(self):
@@ -29,11 +34,11 @@ class OpenFileBtn(tk.Button):
 
         # Clean the dataset
         train_set['abstract'] = train_set['abstract'].str.replace('\W', ' ')  # Removes punctuation
-        train_set['abstract data'] = train_set['abstract data'].str.lower()
-        train_set['abstract data'] = train_set['abstract data'].str.split()
+        train_set['abstract'] = train_set['abstract'].str.lower()
+        train_set['abstract'] = train_set['abstract'].str.split()
 
         # Set up dictionary
-        dictionary = [word for abstract in train_set['abstract data']
+        dictionary = [word for abstract in train_set['abstract']
                                         for word in abstract]
         dictionary = list(set(dictionary))
 
@@ -41,7 +46,7 @@ class OpenFileBtn(tk.Button):
         word_count_per_abstract = {unique_word: [0] * len(train_set['abstract']) for unique_word in dictionary}
 
         # Fill in the word counts
-        for index, abstract in enumerate(train_set['abstract data']):
+        for index, abstract in enumerate(train_set['abstract']):
             for word in abstract:
                 word_count_per_abstract[word][index] += 1
 
@@ -49,5 +54,36 @@ class OpenFileBtn(tk.Button):
         total_word_count = {}
         for word in dictionary:
             total_word_count[word] = sum(word_count_per_abstract[word])
+
+        # Create a dataframe of the word counts for each abstract
+        df = {}
+        for word in dictionary:
+            df[word] = word_count_per_abstract[word]
+        word_counts_df = pd.DataFrame(df)
+
+        # Isolate abstract
+        index = train_set.index
+        A_index = index[train_set['class'] == "A"]
+        A_abstract = word_counts_df.iloc[A_index]
+        B_index = index[train_set['class'] == "B"]
+        B_abstract = word_counts_df.iloc[B_index]
+        E_index = index[train_set['class'] == "E"]
+        E_abstract = word_counts_df.iloc[E_index]
+        V_index = index[train_set['class'] == "V"]
+        V_abstract = word_counts_df.iloc[V_index]
+
+        # Number of Each Abstract
+        n_A = len(A_abstract)
+        n_B = len(B_abstract)
+        n_E = len(E_abstract)
+        n_V = len(V_abstract)
+
+        # Calculate probabilities of each abstract
+        p_A = n_A / len(train_set)
+        p_B = n_B / len(train_set)
+        p_E = n_E / len(train_set)
+        p_V = n_V / len(train_set)
+
+
 
 # Extend a class then override the methods of the child class
